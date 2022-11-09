d218bd1f793f64ff9f0a0b1970bfd46747aff7dd
Bhupesh Kumar <bhupesh.kumar@austere.co.in>
2022-11-09 18:18:29 +0530

    bhupesh: db final changes

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'likes',
     'store',
     'tags'
 ]
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
